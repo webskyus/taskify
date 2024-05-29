f8c57272152580f454aaa9cb4f138a6779ef682f
webskyus <webskyus@gmail.com>
2024-05-29 14:24:45 +0400

    work with supabase schema, create new schema profiles

--- src/app/migrations/schema.ts
+++ src/app/migrations/schema.ts
@@ -4,44 +4,55 @@ import {
     timestamp,
     text,
     jsonb,
+    pgSchema,
 } from 'drizzle-orm/pg-core';
+
+
+const authSchema = pgSchema("auth");
+
+const users = authSchema.table("users", {
+    id: uuid('id')
+        .primaryKey()
+        .notNull()
+})
 
 const workspaces = pgTable('workspaces', {
     id: uuid('id')
         .defaultRandom()
         .primaryKey()
         .notNull(),
-    createdAt: timestamp('created_at', {withTimezone: true, mode: 'string'})
-        .defaultNow()
-        .notNull(),
-    workspaceOwner: uuid('workspace_owner')
-        .notNull(),
     title: text('title')
+        .notNull(),
+    data: text('data')
+        .notNull(),
+    logo: text('logo')
+        .notNull(),
+    banner: text('banner')
+        .notNull(),
+    inTrash: text('in_trash')
         .notNull(),
     iconId: text('icon_id')
         .notNull(),
-    data: text('data'),
-    inTrash: text('in_trash'),
-    logo: text('logo'),
-    bannerUrl: text('banner_url'),
+    workspaceOwner: uuid('workspace_owner')
+        .notNull(),
+    createdAt: timestamp('created_at', {
+        withTimezone: true,
+        mode: 'string',
+    }),
 });
 
-const users = pgTable('users', {
+const profiles = pgTable('profiles', {
     id: uuid('id')
         .primaryKey()
         .notNull(),
+    userId: uuid('user_id')
+        .references(() => users.id)
+        .notNull(),
     fullName: text('full_name'),
-    avatarUrl: text('avatar_url'),
-    billingAddress: jsonb('billing_address'),
-    updatedAt: timestamp('updated_at', {
-        withTimezone: true,
-        mode: 'string'
-    }),
-    paymentMethod: jsonb('payment_method'),
-    email: text('email'),
 });
 
 export {
     workspaces,
+    profiles,
     users
 }
