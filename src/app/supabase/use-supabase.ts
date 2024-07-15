997db8e50bc095e716ebd827cf8e394c50ff3dac
webskyus <webskyus@gmail.com>
2024-07-15 10:03:38 +0400

    fixing console errors

--- src/app/supabase/use-supabase.ts
+++ src/app/supabase/use-supabase.ts
@@ -1,39 +1,42 @@
-import { useEffect, useState } from 'react';
-import { createBrowserClient } from '@supabase/auth-helpers-remix';
-import { useRevalidator } from '@remix-run/react';
-import type { Session } from '@supabase/supabase-js';
-import { Database } from '~/app/supabase/supabase.database';
+import {useEffect, useState} from 'react';
+import {createBrowserClient} from '@supabase/auth-helpers-remix';
+import {useRevalidator} from '@remix-run/react';
+import type {Session} from '@supabase/supabase-js';
+import {Database} from '~/app/supabase/supabase.database';
 
 type SupabaseEnv = {
-	SUPABASE_URL: string;
-	SUPABASE_ANON_KEY: string;
+    SUPABASE_URL: string;
+    SUPABASE_ANON_KEY: string;
 };
 
 type UseSupabase = {
-	env: SupabaseEnv;
-	serverSession: Session | null;
+    env: SupabaseEnv;
+    serverSession: Session | null;
 };
 
-export const useSupabase = ({ env, serverSession }: UseSupabase) => {
-	const [supabase] = useState(() =>
-		createBrowserClient<Database>(env.SUPABASE_URL!, env.SUPABASE_ANON_KEY!)
-	);
-	const serverAccessToken = serverSession?.access_token;
-	const revalidator = useRevalidator();
+export const useSupabase = ({env, serverSession}: UseSupabase) => {
+    const [supabase] = useState(
+        () => createBrowserClient<Database>(
+            env.SUPABASE_URL!,
+            env.SUPABASE_ANON_KEY!,
+        ),
+    );
+    const serverAccessToken = serverSession?.access_token;
+    const revalidator = useRevalidator();
 
-	useEffect(() => {
-		const {
-			data: { subscription },
-		} = supabase.auth.onAuthStateChange((_, session) => {
-			session?.access_token !== serverAccessToken && revalidator.revalidate();
-		});
+    useEffect(() => {
+        const {
+            data: {subscription},
+        } = supabase.auth.onAuthStateChange((_, session) => {
+            session?.access_token !== serverAccessToken && revalidator.revalidate();
+        });
 
-		return () => {
-			subscription.unsubscribe();
-		};
-	}, [supabase.auth, serverAccessToken, revalidator]);
+        return () => {
+            subscription.unsubscribe();
+        };
+    }, [supabase.auth, serverAccessToken, revalidator]);
 
-	return {
-		supabase,
-	};
+    return {
+        supabase,
+    };
 };
