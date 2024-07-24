b937258b9dfc29e3c792915bfc74630c48b19a88
webskyus <webskyus@gmail.com>
2024-07-24 09:02:24 +0400

    work on dnd, add column & list

--- src/features/project-columns/ui/components/project-columns-item/ui/project-columns-item.tsx
+++ src/features/project-columns/ui/components/project-columns-item/ui/project-columns-item.tsx
@@ -6,57 +6,63 @@ import {
 } from '~/shared/ui/dropdown-menu';
 import { Button } from '~/shared/ui/button';
 import { HiOutlineDotsHorizontal } from 'react-icons/hi';
-import {FC} from "react";
-import {ProjectColumn} from "~/routes/dashboard";
-import {FcPlus} from "react-icons/fc";
-import {BsFolderPlus} from "react-icons/bs";
+import { FC } from 'react';
+import { ProjectColumn } from '~/routes/dashboard';
+import { BsFolderPlus } from 'react-icons/bs';
+import {Draggable} from "react-beautiful-dnd";
 
 interface Props {
-	data: ProjectColumn
+	data: ProjectColumn;
+	index: number
 }
 
-const ProjectColumnsItem: FC<Props> = ({data}) => {
-	const {name} = data;
+const ProjectColumnsItem: FC<Props> = ({ data, index }) => {
+	const { name, id } = data;
 
 	return (
-		<article
-			className={
-				`
-					flex flex-col 
-					min-w-[400px] min-h-[200px] h-auto 
-					p-4 mr-4 
-					rounded-sm bg-slate-200 
-					group transition-opacity
-					dark:bg-slate-800 
-				`
-			}>
-			<header className={'flex items-start justify-between'}>
-				<p className={'text-slate-800 dark:text-slate-200'}>
-					{name}
-				</p>
-				<DropdownMenu>
-					<DropdownMenuTrigger>
-						<Button
-							variant={'link'}
-							className={'!h-6 !p-0 hover:opacity-50 transition-opacity'}>
-							<HiOutlineDotsHorizontal size={28} />
-						</Button>
-					</DropdownMenuTrigger>
-					<DropdownMenuContent>
-						<DropdownMenuItem>Edit</DropdownMenuItem>
-						<DropdownMenuItem className={'text-red-400'}>
-							Delete
-						</DropdownMenuItem>
-					</DropdownMenuContent>
-				</DropdownMenu>
-			</header>
+		<Draggable draggableId={id} index={index}>
+			{(provided, snapshot) => {
+				return <article ref={provided.innerRef} {...provided.draggableProps}
+					className={`
+						flex flex-col 
+						min-w-[400px] min-h-[200px] h-auto 
+						p-4 mr-4 
+						rounded-sm bg-slate-200 
+						group transition-opacity
+						dark:bg-slate-800 
+					`}>
+					<header className={'flex items-start justify-between'}>
+						<p className={'text-slate-800 dark:text-slate-200'}>{name}</p>
+						<DropdownMenu>
+							<DropdownMenuTrigger>
+								<Button
+									variant={'link'}
+									className={'!h-6 !p-0 hover:opacity-50 transition-opacity'}>
+									<HiOutlineDotsHorizontal size={28}/>
+								</Button>
+							</DropdownMenuTrigger>
+							<DropdownMenuContent>
+								<DropdownMenuItem>Edit</DropdownMenuItem>
+								<DropdownMenuItem className={'text-red-400'}>
+									Delete
+								</DropdownMenuItem>
+							</DropdownMenuContent>
+						</DropdownMenu>
+					</header>
 
-			<Button className={'mt-auto transition-opacity opacity-0 group-hover:opacity-100 uppercase'}>
-				<BsFolderPlus className={'mr-1'} />
-				New Task
-			</Button>
-		</article>
+					{/*ITEMS LIST*/}
+
+					<Button
+						className={
+							'mt-auto transition-opacity opacity-0 group-hover:opacity-100 uppercase'
+						}>
+						<BsFolderPlus className={'mr-1'}/>
+						New Task
+					</Button>
+				</article>
+			}}
+		</Draggable>
 	);
 };
 
-export { ProjectColumnsItem };
+export {ProjectColumnsItem};
--- src/features/project-columns/ui/project-columns.tsx
+++ src/features/project-columns/ui/project-columns.tsx
@@ -1,53 +1,182 @@
-import { CreateColumnDialog } from '~/features/create-column-dialog';
-import { useLoaderData, useParams } from '@remix-run/react';
-import { loader } from '~/routes/dashboard_.$workspaceId_.$projectId';
-import { ProjectColumnsItem } from '~/features/project-columns/ui/components/project-columns-item';
-import { EmptyResultMessage } from '~/shared/ui/empty-result-message';
-import { ErrorMessage } from '~/shared/ui/error-message';
-import { AnimatePresence } from 'framer-motion';
-import { useGetProjectColumns } from '~/features/project-columns';
-import { getCurrentInfo } from '~/shared/lib/utils';
+import {useState} from "react";
+import {CreateColumnDialog} from '~/features/create-column-dialog';
+import {useLoaderData, useOutletContext, useParams} from '@remix-run/react';
+import {loader} from '~/routes/dashboard_.$workspaceId_.$projectId';
+import {ProjectColumnsItem} from '~/features/project-columns/ui/components/project-columns-item';
+import {EmptyResultMessage} from '~/shared/ui/empty-result-message';
+import {ErrorMessage} from '~/shared/ui/error-message';
+import {useGetProjectColumns} from '~/features/project-columns';
+import {getCurrentInfo} from '~/shared/lib/utils';
+import {DragDropContext, Droppable, DropResult} from 'react-beautiful-dnd';
+import {SupabaseClient} from "@supabase/supabase-js";
+import {ProjectColumn} from "~/routes/dashboard";
+import {reorder, reorderQuoteMap} from "~/shared/lib/utils/dnd";
+
+const getListStyle = (isDraggingOver: boolean) => ({
+    display: 'flex',
+    width: '100%',
+    minHeight: '240px',
+    alignItems: 'items-start',
+    padding: 0,
+    overflow: 'auto',
+});
 
 const ProjectColumns = () => {
-	const { projects } = useLoaderData<typeof loader>();
-	const { projectId } = useParams();
-	const { projectColumns, error } = useGetProjectColumns();
-	const { name, description } = getCurrentInfo(projects, projectId);
+    const {supabase} = useOutletContext<{
+        supabase: SupabaseClient;
+    }>();
 
-	return (
-		<>
-			<header className={'flex items-center justify-between mb-6'}>
-				<header>
-					<h1 className={'mb-1 text-4xl sm:text-6xl font-bold'}>{name}</h1>
-					<p className={'text-md'}>{description}</p>
-				</header>
+    const {projects} = useLoaderData<typeof loader>();
+    const {projectId} = useParams();
 
-				<CreateColumnDialog handleSetId={() => 1} />
+    const {projectColumns, error} = useGetProjectColumns();
+    const {name, description} = getCurrentInfo(projects, projectId);
 
-				{/*TODO ADD FILTERS*/}
-			</header>
+    const [columns, setColumns] = useState(projectColumns);
+    const [ordered, setOrdered] = useState(columns);
 
-			{!projectColumns?.length && !error && <EmptyResultMessage />}
+    const onDragEnd = async (result: DropResult) => {
+        const {destination, source, draggableId, combine} = result;
+        const data = getCurrentInfo(projectColumns, draggableId);
+        const order = Number((Math.random() * 10000000).toFixed());
+        const formData = {...data, id: draggableId};
 
-			{error && <ErrorMessage />}
+        if (!destination) return;
 
-			<section
-				className={`
-                    flex items-start
-                    w-full min-h-[240px] 
-                    overflow-x-auto
-                    transition-all
-                `}>
-				<AnimatePresence>
-					{projectColumns.map(column => {
-						const {id} = column;
+        if (
+            destination.droppableId === source.droppableId &&
+            destination.index === source.index
+        ) return;
 
-						return <ProjectColumnsItem data={column} key={id} />;
-					})}
-				</AnimatePresence>
-			</section>
-		</>
-	);
+        if (result.combine) {
+            if (result.type === 'COLUMN') {
+                const shallow = [...ordered];
+                shallow.splice(source.index, 1);
+                setOrdered(shallow);
+                return;
+            }
+
+            const column = columns[source.droppableId];
+            const withQuoteRemoved = [...column];
+
+            withQuoteRemoved.splice(source.index, 1);
+
+            const orderedColumns = {
+                ...columns,
+                [source.droppableId]: withQuoteRemoved,
+            };
+            setColumns(orderedColumns);
+            return;
+        }
+
+        if (result.type === 'COLUMN') {
+            setOrdered(
+                reorder(
+                    ordered,
+                    source.index,
+                    destination.index
+                )
+            );
+
+            return;
+        }
+
+        const {quoteMap} = reorderQuoteMap({
+            quoteMap: columns,
+            source,
+            destination,
+        });
+
+        setColumns(quoteMap);
+
+        // await updateProjectColumnApi({
+        // 	supabase,
+        // 	formData,
+        // 	order
+        // })
+    };
+
+    return (
+        <>
+            <header className={'flex items-center justify-between mb-6'}>
+                <header>
+                    <h1 className={'mb-1 text-4xl sm:text-6xl font-bold'}>{name}</h1>
+                    <p className={'text-md'}>{description}</p>
+                </header>
+
+                <CreateColumnDialog handleSetId={() => 1}/>
+            </header>
+
+            {!projectColumns?.length && !error && <EmptyResultMessage/>}
+
+            {error && <ErrorMessage/>}
+
+            <DragDropContext onDragEnd={onDragEnd}>
+                <Droppable
+                    droppableId="board"
+                    type="COLUMN"
+                    direction="horizontal"
+                    ignoreContainerClipping={true}
+                    isCombineEnabled={false}
+                >
+                    {(provided, snapshot) => (
+                        <section style={getListStyle(snapshot.isDraggingOver)}
+                                 ref={provided.innerRef}
+                                 {...provided.droppableProps}
+                        >
+                            {
+                                ordered.map((key, index) => {
+                                    return <ProjectColumnsItem key={key} data={columns[index]} index={index}/>
+                                })
+                            }
+                            {provided.placeholder}
+                        </section>
+                    )}
+                </Droppable>
+            </DragDropContext>
+
+
+            {/*<Column*/}
+            {/*	key={key}*/}
+            {/*	index={index}*/}
+            {/*	title={key}*/}
+            {/*	quotes={columns[key]}*/}
+            {/*	isScrollable={withScrollableColumns}*/}
+            {/*	isCombineEnabled={isCombineEnabled}*/}
+            {/*	useClone={useClone}*/}
+            {/*/>*/}
+
+            {/*<DragDropContext onDragEnd={onDragEnd}>*/}
+            {/*	<Droppable*/}
+            {/*		droppableId='droppable'*/}
+            {/*		direction='horizontal'*/}
+            {/*		children={(provided, snapshot) => (*/}
+            {/*			<section*/}
+            {/*				ref={provided.innerRef}*/}
+            {/*				style={getListStyle(snapshot.isDraggingOver)}*/}
+            {/*				{...provided.droppableProps}>*/}
+            {/*				{_projectColumns.map((projectColumn, index) => (*/}
+            {/*					<Draggable*/}
+            {/*						key={projectColumn.id}*/}
+            {/*						draggableId={projectColumn.id}*/}
+            {/*						index={index}>*/}
+            {/*						{(provided, snapshot) => (*/}
+            {/*							<article*/}
+            {/*								ref={provided.innerRef}*/}
+            {/*								{...provided.draggableProps}*/}
+            {/*								{...provided.dragHandleProps}>*/}
+            {/*								<ProjectColumnsItem data={projectColumn} />*/}
+            {/*							</article>*/}
+            {/*						)}*/}
+            {/*					</Draggable>*/}
+            {/*				))}*/}
+            {/*				{provided.placeholder}*/}
+            {/*			</section>*/}
+            {/*		)}*/}
+            {/*	/>*/}
+            {/*</DragDropContext>*/}
+        </>
+    );
 };
 
-export { ProjectColumns };
+export {ProjectColumns};
